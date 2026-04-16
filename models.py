99f7c80a5d348897603ab5c5435a77dcdb580eef
Jelagat2640 <sugutasumpta@gmail.com>
2026-04-16 03:36:12 +0300

    Created models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+inventory = [
+    {
+        "id": 1,
+        "name": "Laptop",
+        "quantity": 10,
+        "price": 999.99
+    },
+    {
+        "id": 2,
+        "name": "Smartphone",
+        "quantity": 20,
+        "price": 499.99
+    },                      
+    
+]
